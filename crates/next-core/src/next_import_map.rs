db583fe91ddf08eb647c2fb42b0faa2a45650d6a
Sam Selikoff <sam.selikoff@gmail.com>
2025-10-04 14:59:43 -0400

    First set of validation tests working in dev/start and webpack/turbo

--- crates/next-core/src/next_import_map.rs
+++ crates/next-core/src/next_import_map.rs
@@ -988,7 +988,7 @@ async fn apply_vendored_react_aliases_server(
         // This is used in the server runtime to import React Server Components.
         alias.extend(fxindexmap! {
             rcstr!("next/navigation") => rcstr!("next/dist/api/navigation.react-server"),
-            rcstr!("next/link") => rcstr!("next/dist/api/link.react-server"),
+            rcstr!("next/link") => rcstr!("next/dist/client/app-dir/link.react-server"),
         });
     }
 
@@ -1018,7 +1018,7 @@ async fn rsc_aliases(
         // This is used in the server runtime to import React Server Components.
         alias.extend(fxindexmap! {
             rcstr!("next/navigation") => rcstr!("next/dist/api/navigation.react-server"),
-            rcstr!("next/link") => rcstr!("next/dist/api/link.react-server"),
+            rcstr!("next/link") => rcstr!("next/dist/client/app-dir/link.react-server"),
         });
     }
 
